f39287582428285e82da2c9c1efd2eed18d692e0
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-05-19 21:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ for(let i = 0; i < 800; i++){
 	});
 	container.appendChild(box);
 }
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+	let color = '#';
+	for(let i = 0; i < 6; i++){
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
