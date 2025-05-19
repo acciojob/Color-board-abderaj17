db2d6c4aafdf403a08704a97769e7abe624db804
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-05-19 21:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 800; i++){
+	const box = document.createElement('div');
+	box.classList.add('square');
+
+	box.addEventListener('mouseover', ()=>{
+		box.style.backgroundColor = getRandomColor();
+
+		setTimeout(() =>{
+			box.style.backgroundColor = '';
+		}, 1000);
+	});
+	container.appendChild(box);
+}
